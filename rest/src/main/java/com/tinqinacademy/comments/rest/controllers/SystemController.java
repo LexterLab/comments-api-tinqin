d233516ed7d93046b19e6c7c54da7d07dec570f4
moldovexc <alexanderparpulansky@gmail.com>
2024-07-13 15:29:31 +0300

    Added api response documentation for system rest APIs

--- rest/src/main/java/com/tinqinacademy/comments/rest/controllers/SystemController.java
+++ rest/src/main/java/com/tinqinacademy/comments/rest/controllers/SystemController.java
@@ -6,6 +6,9 @@ import com.tinqinacademy.comments.api.operations.deletecomment.DeleteRoomComment
 import com.tinqinacademy.comments.api.operations.editusercomment.EditUserCommentInput;
 import com.tinqinacademy.comments.api.operations.editusercomment.EditUserCommentOutput;
 import com.tinqinacademy.comments.rest.utils.PathConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +23,19 @@ public class SystemController {
     private final SystemService systemService;
 
 
+    @Operation(
+            summary = "Edit user's room comment Rest API",
+            description = "Edit user's room comment Rest API is used for admins to edit user's room comments by commentId"
+    )
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "200", description = "HTTP STATUS 200 SUCCESS"),
+            @ApiResponse(responseCode = "400", description = "HTTP STATUS 400 BAD REQUEST"),
+            @ApiResponse(responseCode = "401", description = "HTTP STATUS 401 UNAUTHORIZED"),
+            @ApiResponse(responseCode = "403", description = "HTTP STATUS 403 FORBIDDEN"),
+            @ApiResponse(responseCode = "404", description = "HTTP STATUS 404 NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "HTTP STATUS 500 INTERNAL SERVER ERROR")
+    }
+    )
     @PutMapping(PathConstants.EDIT_USER_COMMENT)
     public ResponseEntity<EditUserCommentOutput> editUserComment(@PathVariable String commentId,
                                                                  @Valid @RequestBody EditUserCommentInput input) {
@@ -34,6 +50,19 @@ public class SystemController {
        return new ResponseEntity<>(systemService.editUserComment(input), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Delete room comment Rest API",
+            description = "Delete room comment Rest API is used for deleting comments by commentId"
+    )
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "200", description = "HTTP STATUS 200 SUCCESS"),
+            @ApiResponse(responseCode = "400", description = "HTTP STATUS 400 BAD REQUEST"),
+            @ApiResponse(responseCode = "401", description = "HTTP STATUS 401 UNAUTHORIZED"),
+            @ApiResponse(responseCode = "403", description = "HTTP STATUS 403 FORBIDDEN"),
+            @ApiResponse(responseCode = "404", description = "HTTP STATUS 404 NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "HTTP STATUS 500 INTERNAL SERVER ERROR")
+    }
+    )
     @DeleteMapping(PathConstants.DELETE_COMMENT)
     public ResponseEntity<DeleteRoomCommentOutput> deleteUserComment(@PathVariable String commentId) {
         DeleteRoomCommentInput input = DeleteRoomCommentInput.builder()
