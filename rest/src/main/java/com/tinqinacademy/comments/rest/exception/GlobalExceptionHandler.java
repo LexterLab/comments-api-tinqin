0f11789cf6a1183b14d13633729e72944f97daff
moldovexc <alexanderparpulansky@gmail.com>
2024-07-29 12:22:35 +0300

    Add resource not found exception to handler for handling missing resources case

--- rest/src/main/java/com/tinqinacademy/comments/rest/exception/GlobalExceptionHandler.java
+++ rest/src/main/java/com/tinqinacademy/comments/rest/exception/GlobalExceptionHandler.java
@@ -1,7 +1,8 @@
 package com.tinqinacademy.comments.rest.exception;
 
 import com.tinqinacademy.comments.api.contracts.ErrorHandlerService;
-import com.tinqinacademy.comments.api.operations.error.ErrorDetails;
+import com.tinqinacademy.comments.api.exceptions.ResourceNotFoundException;
+import com.tinqinacademy.comments.api.operations.error.ErrorOutput;
 import com.tinqinacademy.comments.rest.controllers.HotelController;
 import com.tinqinacademy.comments.rest.controllers.SystemController;
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,8 +23,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<ErrorDetails> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex,
-                                                                              HttpServletRequest request) {
+    public ResponseEntity<ErrorOutput> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex,
+                                                                             HttpServletRequest request) {
         log.error("Request {} raised MethodArgumentNotValidException {}", request.getRequestURL(), ex.getBody());
 
         return new ResponseEntity<>(errorHandlerService.handle(ex), HttpStatus.BAD_REQUEST);
@@ -31,9 +32,15 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<ErrorDetails> handlesServerErrors(Exception ex, HttpServletRequest request) {
+    public ResponseEntity<ErrorOutput> handlesServerErrors(Exception ex, HttpServletRequest request) {
         log.error("Request {} raised ServerErrors {}", request.getRequestURL(), ex.getMessage());
         return new ResponseEntity<>(errorHandlerService.handle(ex), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorOutput> handleResourceNotFound(Exception ex, HttpServletRequest request) {
+        log.error("Request {} raised ResourceNotFoundException {}", request.getRequestURL(), ex.getMessage());
+        return new ResponseEntity<>(errorHandlerService.handle(ex), HttpStatus.NOT_FOUND);
     }
 
 
