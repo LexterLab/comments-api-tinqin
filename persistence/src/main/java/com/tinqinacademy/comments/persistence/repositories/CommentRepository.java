af76d748e9ffdccd5902faa9983176add8466da0
moldovexc <alexanderparpulansky@gmail.com>
2024-07-29 21:04:06 +0300

    Unit tests for get room comments business logic

--- persistence/src/main/java/com/tinqinacademy/comments/persistence/repositories/CommentRepository.java
+++ persistence/src/main/java/com/tinqinacademy/comments/persistence/repositories/CommentRepository.java
@@ -3,9 +3,10 @@ package com.tinqinacademy.comments.persistence.repositories;
 import com.tinqinacademy.comments.persistence.models.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
-    Optional<Comment> findAllByRoomId(UUID roomId);
+  List<Comment> findAllByRoomId(UUID roomId);
 }
