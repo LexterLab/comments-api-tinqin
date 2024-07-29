c0d2a252159b73f0a9d6da10aa40ec6be7acf148
moldovexc <alexanderparpulansky@gmail.com>
2024-07-29 20:02:37 +0300

    Add Jpa Repository for comment entity

---
+++ persistence/src/main/java/com/tinqinacademy/comments/persistence/repositories/CommentRepository.java
@@ -0,0 +1,11 @@
+package com.tinqinacademy.comments.persistence.repositories;
+
+import com.tinqinacademy.comments.persistence.models.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CommentRepository extends JpaRepository<Comment, UUID> {
+    Optional<Comment> findByRoomId(UUID roomId);
+}
