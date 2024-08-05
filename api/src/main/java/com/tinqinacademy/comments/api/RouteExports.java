12bdfce80e2337e1c27bfc1ce38a0274dbbdb84b
moldovexc <alexanderparpulansky@gmail.com>
2024-08-05 12:21:39 +0300

    Export endpoints to feign client

---
+++ api/src/main/java/com/tinqinacademy/comments/api/RouteExports.java
@@ -0,0 +1,9 @@
+package com.tinqinacademy.comments.api;
+
+public class RouteExports {
+    public static final String GET_ROOM_COMMENTS = "GET " + RestAPIRoutes.GET_ROOM_COMMENTS;
+    public static final String LEAVE_ROOM_COMMENT = "POST " + RestAPIRoutes.LEAVE_ROOM_COMMENT;
+    public static final String EDIT_COMMENT = "PUT " + RestAPIRoutes.EDIT_COMMENT;
+    public static final String EDIT_USER_COMMENT = "PATCH " + RestAPIRoutes.EDIT_USER_COMMENT;
+    public static final String DELETE_COMMENT = "DELETE " + RestAPIRoutes.DELETE_COMMENT;
+}
---
+++ restexport/src/main/java/com/tinqinacademy/comments/restexport/restexport/CommentClient.java
@@ -0,0 +1,33 @@
+package com.tinqinacademy.comments.restexport.restexport;
+
+import com.tinqinacademy.comments.api.RouteExports;
+import com.tinqinacademy.comments.api.operations.deletecomment.DeleteRoomCommentOutput;
+import com.tinqinacademy.comments.api.operations.editcomment.EditCommentInput;
+import com.tinqinacademy.comments.api.operations.editcomment.EditCommentOutput;
+import com.tinqinacademy.comments.api.operations.editusercomment.EditUserCommentInput;
+import com.tinqinacademy.comments.api.operations.editusercomment.EditUserCommentOutput;
+import com.tinqinacademy.comments.api.operations.getroomcomments.GetRoomCommentsOutput;
+import com.tinqinacademy.comments.api.operations.leaveroomcomment.LeaveRoomCommentInput;
+import com.tinqinacademy.comments.api.operations.leaveroomcomment.LeaveRoomCommentOutput;
+import feign.Headers;
+import feign.Param;
+import feign.RequestLine;
+
+@Headers({"Content-Type: application/json"})
+public interface CommentClient {
+
+    @RequestLine(RouteExports.GET_ROOM_COMMENTS)
+    GetRoomCommentsOutput getRoomComments(@Param String roomId);
+
+    @RequestLine(RouteExports.LEAVE_ROOM_COMMENT)
+    LeaveRoomCommentOutput leaveRoomComment(@Param String roomId, LeaveRoomCommentInput input);
+
+    @RequestLine(RouteExports.EDIT_COMMENT)
+    EditCommentOutput editComment(@Param String commentId, EditCommentInput input);
+
+    @RequestLine(RouteExports.EDIT_USER_COMMENT)
+    EditUserCommentOutput editUserComment(@Param String commentId, EditUserCommentInput input);
+
+    @RequestLine(RouteExports.DELETE_COMMENT)
+    DeleteRoomCommentOutput deleteRoomComment(@Param String commentId);
+}
